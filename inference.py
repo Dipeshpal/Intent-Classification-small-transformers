21290bedb01ed973930ce2fc846a6bbb18547ee8
Dipesh Paul <dipeshpal17@gmail.com>
2023-01-19 20:45:52 +0530

    Fix and Update

--- inference.py
+++ inference.py
@@ -1,20 +1,19 @@
 import transformers
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
 
 transformers.logging.set_verbosity_error()
 
-from transformers import AutoTokenizer, AutoModel, AutoModelForSequenceClassification
-
 # dipesh/Intent-Classification-bart-large-mnli
 # dipesh/autotrain-intent-classification-2953985512
-# dipesh/Intent-Classification-roberta-base-finetuned-intent
+# dipesh/Intent-Classification-roberta-base-finetuned-intent  # RECOMMENDED
 tokenizer = AutoTokenizer.from_pretrained('dipesh/Intent-Classification-roberta-base-finetuned-intent')
 model = AutoModelForSequenceClassification.from_pretrained("dipesh/Intent-Classification-roberta-base-finetuned-intent")
 
-from transformers import pipeline
 
 nlp = pipeline('zero-shot-classification', model=model, tokenizer=tokenizer)
 
-sequence = "how are you"
 classes = ['asking date', 'asking time', 'tell me joke', 'tell me news',
            'asking weather', 'tell me about', 'open website',
            'play on youtube', 'send whatsapp message', 'send email',
@@ -23,7 +22,26 @@ classes = ['asking date', 'asking time', 'tell me joke', 'tell me news',
            'covid cases', 'play games', 'places near me', 'i am bored',
            'volume control', 'what can you do']
 
-results = nlp(sequence, classes)
-print(results)
 
-print(results['labels'][0])
+# TESTING ON SAME DATASET, YOU CAN USE DIFFERENT ONE
+df = pd.read_csv("data_generator/train.csv")
+
+
+wrong = 0
+correct = 0
+total = 0
+for index, row in df.iterrows():
+    sequence = row['text']
+    results = nlp(sequence, classes)
+    if results['labels'][0] == row['label']:
+        print("TRUE: ", sequence, "| ", results['labels'][0], ": ", results['scores'][0], "| ", row['label'])
+        correct += 1
+    else:
+        print("FALSE: ", sequence, "| ", results['labels'][0], ": ", results['scores'][0], "| ", row['label'])
+        wrong += 1
+    total += 1
+
+print("Correct: ", correct)
+print("Wrong: ", wrong)
+print("Total: ", total)
+print("Accuracy: ", correct/total)
--- zero-shot-finetuning-train.py
+++ zero-shot-finetuning-train.py
@@ -1,49 +1,60 @@
+import torch
 import shutup
+import os
+from datetime import datetime
 from transformers import logging
 from datasets import load_dataset
 from setfit import SetFitModel, SetFitTrainer
-import os
-import torch
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
 
 shutup.please()
 logging.set_verbosity_error()
 
-# Set up the dataset
-dataset_id = "dipesh/Intent-Classification-Commands"
-# set up pre-trained model
-model_id = "facebook/bart-large-mnli"  # "typeform/distilbert-base-uncased-mnli" #facebook/bart-large-mnli
+epochs = 2
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
 
-# your model name
-YOUR_MODEL_NAME = "Intent-Classification-bart-large-mnli"
+
+def save_and_evaluate_model(trainer):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if not os.path.exists('model/'):
+        os.mkdir('model/')
+    save_directory = f"model/{timestamp}-Intent-Classification-roberta-base-finetuned-intent"
+    os.mkdir(save_directory)
+    trainer.model._save_pretrained(save_directory=save_directory)
+    print(f"Model saved to {save_directory}")
+
+    zeroshot_metrics = trainer.evaluate()
+    print(zeroshot_metrics)
+
+
+dataset_id = "dipesh/Intent-Classification-Commands-large"
+model_id = "dipesh/Intent-Classification-roberta-base-finetuned-intent"  # "typeform/distilbert-base-uncased-mnli" #facebook/bart-large-mnli #zhiyil/roberta-base-finetuned-intent
 
 reference_dataset = load_dataset(dataset_id)
-reference_dataset = reference_dataset.rename_column("labels", "label")
+reference_dataset = reference_dataset.rename_column("intent", "label")
 print(reference_dataset)
 
-model = SetFitModel.from_pretrained(model_id)
-trainer = SetFitTrainer(
-    model=model,
-    train_dataset=reference_dataset["train"],
-    eval_dataset=reference_dataset["test"]
-)
 
-trainer.train()
-zeroshot_metrics = trainer.evaluate()
-print(zeroshot_metrics)
+def train_and_load_model(model_id):
+    model = SetFitModel.from_pretrained(model_id)
+    trainer = SetFitTrainer(
+        model=model,
+        train_dataset=reference_dataset["train"],
+        eval_dataset=reference_dataset["test"],
+        batch_size=64,
+        num_iterations=16,  # The number of text pairs to generate for contrastive learning
+        num_epochs=1,  # The number of epochs to use for constrastive learning
+    )
 
-# save the model
-if not os.path.exists(YOUR_MODEL_NAME):
-    os.mkdir(YOUR_MODEL_NAME)
+    trainer.train()
+    return trainer
 
-save_directory = YOUR_MODEL_NAME
-trainer.model._save_pretrained(save_directory=save_directory)
 
-# make predictions
-print("Making predictions")
-print("what is the weather like today?: ", trainer.model.predict(["what is the weather like today?"]))
+for i in range(epochs):
+    trainer = train_and_load_model(model_id)
+    save_and_evaluate_model(trainer)
+    print('Checking ["how are you", "what is the time now", "who is narendra modi"]: ',
+          trainer.model.predict(["how are you", "what is the time now", "who is narendra modi"]))
+    print(trainer.model.push_to_hub('Intent-Classification-roberta-base-finetuned-intent'))
 
-print("YOUR MODEL NAME: ", YOUR_MODEL_NAME, "saved in ", save_directory, "directory")
-print("PUSH TO HUGGING FACE...")
-print(trainer.model.push_to_hub(YOUR_MODEL_NAME))
+print("Done")
